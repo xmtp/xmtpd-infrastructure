f5005bad3e4be2a099b7731cdd35a25d0d1eaeee
Richard Hua <rich@rkhua.com>
2024-10-30 12:50:19 -0700

    Remove users

--- terraform/examples/validation-service-aws/refactor.tf
+++ terraform/examples/validation-service-aws/refactor.tf
@@ -4,33 +4,6 @@ moved {
 }
 
 moved {
-  from = aws_iam_group.developers
-  to   = module.grant_user_access.aws_iam_group.developers
-}
-
-moved {
-  from = aws_iam_group_policy_attachment.power_user
-  to   = module.grant_user_access.aws_iam_group_policy_attachment.power_user
-}
-
-
-moved {
-  from = aws_iam_user.developer
-  to   = module.grant_user_access.aws_iam_user.developer
-}
-
-
-moved {
-  from = aws_iam_user_group_membership.developers
-  to   = module.grant_user_access.aws_iam_user_group_membership.developers
-}
-
-moved {
-  from = module.enforce_mfa
-  to   = module.grant_user_access.module.enforce_mfa
-}
-
-moved {
   from = module.mls_validation_service
   to   = module.xmtp_node_us_east_2.module.mls_validation_service
 }
