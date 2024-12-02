b59665d953e143488252268e6e813063d4162290
Martin Kysel <code@martinkysel.com>
2024-12-02 12:57:55 -0500

    make payer happy

--- test/xmtp-helm/base_payer_test.go
+++ test/xmtp-helm/base_payer_test.go
@@ -11,10 +11,25 @@ func TestKubernetesBasicPayerInstall(t *testing.T) {
 	defer testlib.VerifyTeardown(t)
 	defer testlib.Teardown(testlib.TEARDOWN_GLOBAL)
 
+	namespace := testlib.CreateRandomNamespace(t, 2)
+
 	options := helm.Options{
-		SetValues: testlib.GetDefaultSecrets(t),
+		SetValues: map[string]string{},
+	}
+
+	// technically the payer does NOT require a DB
+	// but XMTPD 0.1.1 has some incorrect defaults
+	// which will prevent the start of the service without DB connectivity
+	defer testlib.Teardown(testlib.TEARDOWN_DATABASE)
+	_, _, db := testlib.StartDB(t, &options, namespace)
+
+	secrets := testlib.GetDefaultSecrets(t)
+	secrets["env.secret.XMTPD_DB_WRITER_CONNECTION_STRING"] = db.ConnString
+
+	options = helm.Options{
+		SetValues: secrets,
 	}
 
 	defer testlib.Teardown(testlib.TEARDOWN_PAYER)
-	testlib.StartPayer(t, &options, 2, "")
+	testlib.StartPayer(t, &options, 2, namespace)
 }
