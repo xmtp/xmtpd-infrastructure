0f888acae5e30829064ab00d0496fa5b30096c64
Pavel Zaborskii <pashu13@list.ru>
2025-01-15 22:39:26 +0300

    Fix comment errors and bug report (#24)
    Fix typos

--- terraform/aws/fargate-task-definition/_outputs.tf
+++ terraform/aws/fargate-task-definition/_outputs.tf
@@ -1,4 +1,4 @@
 output "task_definition_arn" {
-  description = "The ARN of the task defintion"
+  description = "The ARN of the task definition"
   value       = aws_ecs_task_definition.task.arn
 }
